014d6db2fb8c6be8dc83693f4422ff4340acf029
Minghui Zhao <maggiezhao2008@gmail.com>
2020-12-18 19:41:11 -0800

    deploy

---
+++ precache-manifest.879b97471558f5ae14170925cc37ebc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8480fa01763d32a39ed595ff61f877bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36f0e3b308c7e9b78ce9",
+    "url": "/static/css/main.bb51d353.chunk.css"
+  },
+  {
+    "revision": "972eff50d6593e1ed11f",
+    "url": "/static/js/2.5b97f4a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5b97f4a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36f0e3b308c7e9b78ce9",
+    "url": "/static/js/main.96ed9599.chunk.js"
+  },
+  {
+    "revision": "86d5ce63a8725302eb5d",
+    "url": "/static/js/runtime-main.ec220941.js"
+  }
+]);
